9e1637b34f38dc2298114b696a13341764406db0
harryuan65 <harryuan.65@gmail.com>
2022-03-12 21:45:14 +0800

    chore: build dist for prod

--- rollup.config.js
+++ rollup.config.js
@@ -2,18 +2,20 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 import pkg from './package.json';
+import css from 'rollup-plugin-import-css';
 
 export default [
   {
     input: 'src/index.js', // your entry point
     output: {
-      name: 'ruby-annotation', // package name
+      name: pkg.name, // package name
       file: pkg.browser,
       format: 'umd',
     },
     plugins: [
       resolve(),
       commonjs(),
+      css(),
       babel({
         exclude: ['node_modules/**'],
       }),
@@ -23,9 +25,10 @@ export default [
     input: 'src/index.js', // your entry point
     output: [
       { file: pkg.main, format: 'cjs' },
-      { file: pkg.module, format: 'es' },
+      { file: pkg.module, format: 'esm' },
     ],
     plugins: [
+      css(),
       babel({
         exclude: ['node_modules/**'],
       }),
